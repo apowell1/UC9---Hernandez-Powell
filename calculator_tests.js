cb2aa4d1e7b6897a0a021025c50518efe4669b4a
Aaron M Powell <ap59264@srcs.loc>
2017-02-01 14:08:00 -0800

    us 2, 4, 6, 8

--- calculator_tests.js
+++ calculator_tests.js
@@ -3,6 +3,7 @@ QUnit.test( "Add digits test", function( assert ) {
     addDigit('1');
     addDigit('2');
     assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
+    allClear();
 });
 
 // Test adding one, then two decimals
@@ -11,30 +12,54 @@ QUnit.test( "Add decimal test", function( assert ) {
     addDigit('2');
     addDecimal();
     assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
+    allClear();
 });
 //US1: As a user, I want to be able to press a button and change the sign of the current number that I have inputted into the calculator.
 
 
 //US2: As a user I want to be able to press a button and change the inputed number on the calculator's screen to change to a percentage.
 
-
+QUnit.test( "Percentage test", function( assert ) {
+    addDigit('2');
+    percentage();
+    assert.equal(document.getElementById("screen").value, "0.02", "Passed - Expected 0.02");
+    allClear();
+});
 //US3: As a user, I want to be able to calculate the inverse of a number simply by pressing a button.
 
 
 //US4: As a user I want to be able to calculate the factorial of a number by simply pressing a button.
 
-
+QUnit.test( "Factorial test", function( assert ) {
+    addDigit('4');
+    factorial();
+    assert.equal(document.getElementById("screen").value, "24", "Passed - Expected 24");
+    allClear();
+});
 //US5: As a user I want to be able to calculate the square root of a number by simply pressing a button.
 
 
 //US6: As a user I want to be able to calculate the square of a number by simply pressing a button.
 
-
+QUnit.test( "Square test", function( assert ) {
+    addDigit('4');
+    square();
+    assert.equal(document.getElementById("screen").value, "16", "Passed - Expected 16");
+    allClear();
+});
 //US7: As a user who sometimes makes mistakes when pressing buttons on the keypad, I want to be able to press a button that clears my current input, but not the stored procedure.
 
 
 //US8: Bug Alert! There is a bug in the calculator app! As a careless user I want to be told that I just tried to divide by zero, which I should be told is not allowed.
 
 
+QUnit.test( "Divide by zero test", function( assert ) {
+    addDigit('6');
+    storeOperator('/');
+    addDigit('0');
+    calculate();
+    assert.equal(document.getElementById("screen").value, "CANNOT DIVIDE BY 0", "Passed - Expected CANNOT DIVIDE BY 0");
+    allClear();
+});
 //US9: Bug Alert! As an easily confused user I don't want to be able to type numbers into the screen that causes some of the numbers to disappear off the screen, thus confusing me about what I actually typed.
 
