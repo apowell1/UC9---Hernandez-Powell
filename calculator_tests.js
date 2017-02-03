25d09cd05035b906ebd5116461b5d435d9965efe
Richard T Hernandez-Hernandez <rh47199@srcs.loc>
2017-02-03 12:54:07 -0800

    im a rock

--- calculator_tests.js
+++ calculator_tests.js
@@ -14,6 +14,7 @@ QUnit.test( "Add decimal test", function( assert ) {
     assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
     allClear();
 });
+
 //US1: As a user, I want to be able to press a button and change the sign of the current number that I have inputted into the calculator.
 QUnit.test( "Change the sign of the number test", function( assert ){
     addDigit('2');
@@ -23,13 +24,13 @@ QUnit.test( "Change the sign of the number test", function( assert ){
 });
 
 //US2: As a user I want to be able to press a button and change the inputed number on the calculator's screen to change to a percentage.
-
 QUnit.test( "Percentage test", function( assert ) {
     addDigit('2');
     percentage();
     assert.equal(document.getElementById("screen").value, "0.02", "Passed - Expected 0.02");
     allClear();
 });
+
 //US3: As a user, I want to be able to calculate the inverse of a number simply by pressing a button.
 QUnit.test( "Calculate the Inverse of a Numer", function( assert ){
    addDigit('4');
@@ -39,13 +40,13 @@ QUnit.test( "Calculate the Inverse of a Numer", function( assert ){
 });
 
 //US4: As a user I want to be able to calculate the factorial of a number by simply pressing a button.
-
 QUnit.test( "Factorial test", function( assert ) {
     addDigit('4');
     factorial();
     assert.equal(document.getElementById("screen").value, "24", "Passed - Expected 24");
     allClear();
 });
+
 //US5: As a user I want to be able to calculate the square root of a number by simply pressing a button.
 QUnit.test( "Square Root Test", function ( assert ){
     addDigit('16');
@@ -55,13 +56,13 @@ QUnit.test( "Square Root Test", function ( assert ){
 });
 
 //US6: As a user I want to be able to calculate the square of a number by simply pressing a button.
-
 QUnit.test( "Square test", function( assert ) {
     addDigit('4');
     square();
     assert.equal(document.getElementById("screen").value, "16", "Passed - Expected 16");
     allClear();
 });
+
 //US7: As a user who sometimes makes mistakes when pressing buttons on the keypad, I want to be able to press a button that clears my current input, but not the stored procedure.
 QUnit.test( "Clear Current Input Test", function (assert ){
    addDigit('1');
@@ -71,8 +72,6 @@ QUnit.test( "Clear Current Input Test", function (assert ){
 });
 
 //US8: Bug Alert! There is a bug in the calculator app! As a careless user I want to be told that I just tried to divide by zero, which I should be told is not allowed.
-
-
 QUnit.test( "Divide by zero test", function( assert ) {
     addDigit('6');
     storeOperator('/');
@@ -81,11 +80,41 @@ QUnit.test( "Divide by zero test", function( assert ) {
     assert.equal(document.getElementById("screen").value, "CANNOT DIVIDE BY 0", "Passed - Expected CANNOT DIVIDE BY 0");
     allClear();
 });
-//US9: Bug Alert! As an easily confused user I don't want to be able to type numbers into the screen that causes some of the numbers to disappear off the screen, thus confusing me about what I actually typed.
 
+//US9: Bug Alert! As an easily confused user I don't want to be able to type numbers into the screen that causes some of the numbers to disappear off the screen, thus confusing me about what I actually typed.
 QUnit.test( "Overload test", function( assert ) {
     addDigit('555555555555555555555');
     addDigit('1');
     assert.equal(document.getElementById("screen").value, "OVERLOAD ERROR", "Passed - Expected OVERLOAD ERROR");
+    allClear();
+});
+
+//US10:
+
+
+//US11: Extra Challenge: Add the ability to store a number in memory with a "m+", "m-", "mr" and "mc".
+QUnit.test( "Use all memory function test", function ( assert ) {
+   addDigit('6');
+    ms();
+    mr();
+    mc();
+    ms();
+    mr();
+    mPlus();
+    mMinus();
+    assert.equal(document.getElementById("screen").value, "-6", "Passed - Expected -6");
+    allClear();
+});
+
+//US12:
+
+
+//US13: Super Duper Challenge: Add the ability to work with exponents!
+QUnit.test( "Use expononets test", function (assert) {
+    addDigit('5');
+    storeOperator('x^y')
+    addDigit('2');
+    calculate();
+    assert.equal(document.getElementById("screen").value, "25", "Passed - Expected 25");
     allClear();
 });
